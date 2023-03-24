556eead9ad7bc406fbcdc21c7908a2f27f8c4a9e
Eduardo Pacheco <eduardoapp.97@gmail.com>
2023-03-24 17:00:59 -0400

    Add ClipperProtocolDeposit contract events

---
+++ src/entities/Fee.ts
@@ -0,0 +1,54 @@
+import { BigInt, ethereum } from "@graphprotocol/graph-ts";
+
+import { Fee } from "../../types/schema";
+import { BIG_INT_EIGHTEEN, BIG_INT_ONE } from "../constants";
+import { clipperExchangeAddress } from "../constants/addresses";
+import { loadFeeSplitStatus } from "../entities";
+import { convertTokenToDecimal } from "../utils";
+import {
+  getCurrentPoolLiquidity,
+  getPoolTokenSupply,
+  getPoolTokenValue,
+} from "../utils/pool";
+
+export function createFee(
+  event: ethereum.Event,
+  entitledFeesInDollars: BigInt,
+  averagePoolBalanceInDollars: BigInt,
+  tokensTransferred: BigInt
+): Fee {
+  let liquidity = getCurrentPoolLiquidity(clipperExchangeAddress);
+  let poolTokenSupply = getPoolTokenSupply(clipperExchangeAddress);
+  let fee = new Fee(event.transaction.hash.toHexString());
+  fee.hash = event.transaction.hash;
+  fee.avgPoolBalanceInDollars = averagePoolBalanceInDollars.toBigDecimal();
+  fee.entitledValueInUsd = entitledFeesInDollars.toBigDecimal();
+
+  let transferredPoolTokens = tokensTransferred;
+
+  let transferredPoolTokensValueInUsd = getPoolTokenValue(
+    liquidity,
+    poolTokenSupply,
+    transferredPoolTokens
+  );
+
+  fee.valueInUsd = transferredPoolTokensValueInUsd;
+  fee.poolTokens = convertTokenToDecimal(
+    transferredPoolTokens,
+    BIG_INT_EIGHTEEN
+  );
+  fee.timestamp = event.block.timestamp;
+
+  let contractStatus = loadFeeSplitStatus(event.address);
+  contractStatus.feesTakenTransactionCount = contractStatus.feesTakenTransactionCount.plus(
+    BIG_INT_ONE
+  );
+  contractStatus.totalFeesTaken = contractStatus.totalFeesTaken.plus(
+    convertTokenToDecimal(transferredPoolTokens, BIG_INT_EIGHTEEN)
+  );
+
+  contractStatus.save();
+  fee.save();
+
+  return fee;
+}
